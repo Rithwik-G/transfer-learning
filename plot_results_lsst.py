393da4b82e6632665388e6d20346c15a105f40f3
Rithwik-G <86076806+Rithwik-G@users.noreply.github.com>
2025-07-13 18:36:36 -0700

    Update plot_results_lsst.py

--- plot_results_lsst.py
+++ plot_results_lsst.py
@@ -8,23 +8,7 @@ from utils.utils import Results
 
 plt.figure(figsize=(8, 6))
 
-storage_path = "/ocean/projects/phy240020p/rgupta9/transfer_learning"
-
-# files = ['lsst/lsst_ft_lim.pkl', 'lsst/lsst_direct.pkl'] # 'lsst/lsst_ft_all.pkl'
-
-# files = ['bts/bts_direct_five.pkl', 'bts/bts_ft_output_five.pkl', 'bts/bts_ft_last_two_five1.pkl']#, 'bts/bts_ft_all.pkl', 'bts/bts_ft_last_two.pkl']
-# names = ['Transfer Learning', 'Direct Training']
-
-# files = ['bts_ft_output_five1', 'bts_ft_last_two_five1', 'bts_ft_all_five1']#, 'bts_direct_five1'] # 'bts_ft_last_two_five1', 'bts_ft_output_five1', 
-# names = ['Output', 'Last Two Layers', 'Fully Unfrozen']
-# files = [ ['lsst/lsst_ft_suff_new'], ['lsst/lsst_direct_new']] #, ['lsst/lsst_ft_only_output'], ['lsst/lsst_ft_only_output_replace']] # ['lsst/lsst_ft_all2', 'lsst/lsst_ft_pref_suff2', 'lsst/lsst_ft_pref2']
-# names = ['Transfer Learning', 'Direct Training', 'Transfer Learning Output Only', 'Output Replaced'] # 'Transfer Learning', 'Direct Training', 'Fully Unfrozen']
-
-# files = [ ['lsst/class_change_ft_'], ['lsst/class_change_direct'], ['lsst/class_change_ft_gru']] #, ['lsst/lsst_ft_only_output'], ['lsst/lsst_ft_only_output_replace']] # ['lsst/lsst_ft_all2', 'lsst/lsst_ft_pref_suff2', 'lsst/lsst_ft_pref2']
-# names = ['Transfer Learning', 'Direct Training', 'Transfer Learning Output Only', 'Output Replaced'] # 'Transfer Learning', 'Direct Training', 'Fully Unfrozen']
-
-# files = [['lsst/lsst_ft_pref_suff'], ['lsst/lsst_direct_new']]
-# names = ['Transfer Learning', 'Direct Training']
+storage_path = "rgupta9/transfer_learning"
 
 files = [['lsst/lsst_ft_pref_suff'], ['lsst/lsst_ft_output'], ['lsst/lsst_ft_only_output']]
 names = ['GRU and Output', 'Output Only', 'Full Model']
@@ -92,21 +76,15 @@ def samples_to_time(x):
 def time_to_samples(x):
     return np.array(x) * total / total_time
 
-# secax = ax.secondary_xaxis('top', functions=(samples_to_time, time_to_samples))
-# secax.set_xlabel("Observation Time (Days)", fontsize=15)
-# plt.xscale('log')
 
 plt.legend(fontsize=15)
-# plt.title("LSST Sims Performance", fontsize=20)
 plt.xlabel("Training Samples", fontsize=15)
 plt.ylabel("Classification AUROC", fontsize=15)
-# os.makedirs("figures", exist_ok=True)
-# plt.ylim(0.4, 1.0)
+os.makedirs("figures", exist_ok=True)
 plt.grid()
 plt.savefig("final_figures/lsst_hyper.pdf", bbox_inches='tight')
 plt.show()
 plt.clf()
-# assert(False)
 
 
 best = 'lsst/lsst_ft_pref_suff'
@@ -128,46 +106,17 @@ for limit in limits:
 for ind, cl in enumerate(by_class.keys()):
     err = [np.median(np.abs(res[limit]['class_based'][cl] - np.median(res[limit]['class_based'][cl]))) for limit in limits]
     perf = by_class[cl]
-    # plt.errorbar(limits, perf, yerr=err, fmt='o', capsize=3, capthick=1, elinewidth=1, label=cl, c=colors[ind])
     perf = np.array(perf)
     err = np.array(err)
     plt.plot(limits, perf, marker='.', label= cl, color=colors[ind])
     plt.fill_between(limits, perf - err, perf + err, alpha=0.1, color=colors[ind])
 
 plt.legend(fontsize=10, ncol=2)
-# plt.title("LSST Sims Class-Based Performance", fontsize=20)
+
 plt.xlabel("Training Samples", fontsize=15)
 plt.ylabel("Classification AUROC", fontsize=15)
-# plt.ylim(0.4, 1.0)
+
 os.makedirs("figures", exist_ok=True)
 plt.grid()
 plt.savefig("final_figures/lsst_cls.pdf", bbox_inches='tight')
 plt.show()
-
-# m = 'macro'
-# fname = "lsst_ft_perf.pkl"
-
-
-# res = load(os.path.join(storage_path, fname))
-
-
-# limits = list(res.keys())
-# perf = []
-# err = []
-# for limit in limits:
-#     perf.append(np.mean(res[limit][m]))
-#     err.append(np.std(res[limit][m]))
-
-# plt.figure(figsize=(8, 6))
-# plt.errorbar(limits, perf, yerr=err, fmt='o', capsize=5, capthick=2, elinewidth=2, label='Transfer Learning')
-
-# fname = "bts_direct_perf_5.pkl"
-# res = load(os.path.join(storage_path, fname))
-# limits = list(res.keys())
-# perf = []
-# err = []
-# for limit in limits:
-#     perf.append(np.mean(res[limit][m]))
-#     err.append(np.std(res[limit][m]))
-
-# plt.errorbar(limits, perf, yerr=err, fmt='o', capsize=5, capthick=2, elinewidth=2, label='Direct Training')
